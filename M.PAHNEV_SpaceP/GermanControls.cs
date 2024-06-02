72cd32daa21be896dbfd5df7094450a01550352c
Martin Pahnev <mitkomarti1@gmail.com>
2024-06-02 16:20:12 +0300

    fix csv files, add input language support for german and english

--- M.PAHNEV_SpaceP/GermanControls.cs
+++ M.PAHNEV_SpaceP/GermanControls.cs
@@ -11,11 +11,20 @@ namespace M.PAHNEV_SpaceP
 
         const string GermanExitMessage = "Vielen Dank für die Nutzung unseres Systems. Auf Wiedersehen!";
 
+        const string GermanPathToFolderMessage = "Bitte geben Sie den Pfad zu dem Ordner mit den Wetterberichten ein.";
+        const string GermanSenderEmailMessage = "Bitte geben Sie Ihre E-Mail-Adresse ein.";
+        const string GermanSenderPasswordMessage = "Bitte geben Sie Ihr E-Mail-Passwort ein.";
+        const string GermanRecipientEmailMessage = "Bitte geben Sie die E-Mail-Adresse des Empfängers ein.";
+
 
         public GermanControls() 
 		{
-			this.StartingMessage = GermanStartingMessage;
-			this.ExitMessage = GermanExitMessage;
+			StartingMessage = GermanStartingMessage;
+            ExitMessage = GermanExitMessage;
+            PathToFolderMessage = GermanPathToFolderMessage;
+            SenderEmailMessage = GermanSenderEmailMessage;
+            SenderPasswordMessage = GermanSenderPasswordMessage;
+            RecipientEmailMessage = GermanRecipientEmailMessage;
 		}
     }
 }
